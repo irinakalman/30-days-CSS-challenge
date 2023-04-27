4c547066ceb558ebffc816d686bc7aaa04a2a897
irinakalman <81618370+irinakalman@users.noreply.github.com>
2023-04-27 19:38:34 +0300

    2nd day of CSS challenge: Neumorphism Login and Sign Up Form

---
+++ script.js
@@ -0,0 +1,23 @@
+let buttonSwap1 = document.querySelector(".js-button-swap1");
+let buttonSwap2 = document.querySelector(".js-button-swap2");
+console.log(buttonSwap1);
+console.log(buttonSwap2);
+// let leftPart = document.getElementsByClassName("left-part");
+// let rightPart = document.getElementsByClassName("right-part");
+
+buttonSwap1.addEventListener("click", function(){
+    console.log("click");
+buttonSwap1.closest('.left-part').classList.toggle('swap-left');
+buttonSwap1.textContent = 'Sign Up';
+buttonSwap2.closest(".right-part").classList.toggle("swap-right");
+buttonSwap2.textContent = "Sign In";
+})
+
+buttonSwap2.addEventListener("click", function(){
+    console.log("click");
+buttonSwap1.closest('.left-part').classList.remove('swap-left');
+buttonSwap1.textContent = 'Sign In';
+buttonSwap2.closest(".right-part").classList.remove("swap-right");
+buttonSwap2.textContent = "Sign Up";
+
+})
