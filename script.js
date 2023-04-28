b98bc0463ccb3df3b8f880108c529962a2de9226
irinakalman <81618370+irinakalman@users.noreply.github.com>
2023-04-28 11:33:20 +0300

    fix

--- script.js
+++ script.js
@@ -5,19 +5,18 @@ console.log(buttonSwap2);
 // let leftPart = document.getElementsByClassName("left-part");
 // let rightPart = document.getElementsByClassName("right-part");
 
-buttonSwap1.addEventListener("click", function(){
-    console.log("click");
-buttonSwap1.closest('.left-part').classList.toggle('swap-left');
-buttonSwap1.textContent = 'Sign Up';
-buttonSwap2.closest(".right-part").classList.toggle("swap-right");
-buttonSwap2.textContent = "Sign In";
-})
+buttonSwap1.addEventListener("click", function () {
+  console.log("click");
+  buttonSwap1.closest(".left-part").classList.toggle("swap-left");
+  buttonSwap1.textContent = "Sign Up";
+  buttonSwap2.closest(".right-part").classList.toggle("swap-right");
+  buttonSwap2.textContent = "Sign In";
+});
 
-buttonSwap2.addEventListener("click", function(){
-    console.log("click");
-buttonSwap1.closest('.left-part').classList.remove('swap-left');
-buttonSwap1.textContent = 'Sign In';
-buttonSwap2.closest(".right-part").classList.remove("swap-right");
-buttonSwap2.textContent = "Sign Up";
-
-})
+buttonSwap2.addEventListener("click", function () {
+  console.log("click");
+  buttonSwap2.closest(".right-part").classList.toggle("swap-right");
+  buttonSwap2.textContent = "Sign Up";
+  buttonSwap1.closest(".left-part").classList.toggle("swap-left");
+  buttonSwap1.textContent = "Sign In";
+});
